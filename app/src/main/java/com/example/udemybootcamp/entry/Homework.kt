9b3a41e3d253f6e1f71ceeb78b39051905a6aa6d
Hakan <139192890+hakan315@users.noreply.github.com>
2026-04-13 20:51:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/udemybootcamp/entry/Homework.kt
@@ -0,0 +1,46 @@
+package com.example.udemybootcamp.entry
+
+
+fun main() {
+
+    val district = "Kaynaşlı"
+    println("ilçe adı : $district")
+
+    val continent : String = "Europe"
+    println("Ülke adı : $continent")
+
+    val fax : Int = 2125550000
+    println("Faks numarası : $fax")
+
+    val postalCode = 34330
+    println("Posta kodu : $postalCode")
+
+    val instagramAddress = "@hakan.ağca"
+    println("İnstagram adresi : $instagramAddress")
+
+    val department : String = "Halkla İlişkiler"
+    println("Departman adı : $department")
+
+    val productQuantity = "50 adet"
+    println("Ürün adedi : $productQuantity")
+
+    val customerSurname = "Ağca"
+    println("Müşteri soyadı : $customerSurname")
+
+    val paymentAmount = "50 tl"
+    println("Ödeme tutarı : $paymentAmount")
+
+    val dateOfBirth = "13/07/200"
+    println("Doğum tarihi : $dateOfBirth")
+
+    val debt : Int = 500
+    println("Borç : $debt tl")
+
+    val maritalStatus = "Bekar"
+    println("Medeni durum : $maritalStatus")
+
+    val videoCommentary : String = "Harika"
+    println("Video yorumu : $videoCommentary")
+
+
+}
---
+++ app/src/main/java/com/example/udemybootcamp/entry/Variables.kt
@@ -0,0 +1,90 @@
+package com.example.udemybootcamp.entry
+
+
+fun main() {
+    // println("Merhaba dünya")
+    // sadece println varken neden hata veri abine sor
+
+    var studentName = "Ahmet"
+    var studentAge = 23
+    var studentSize = 1.98
+    var studentInitial = 'A'
+    var continuity = true
+
+    println(studentName)
+    println(studentAge)
+    println(studentSize)
+    println(studentInitial)
+    println(continuity)
+
+
+    var product_id: Int = 3416
+    var product_name: String = "Macbook Pro"
+    var product_stock: Int = 100
+    var product_price: Double = 42999.99
+    var product_supplier: String = "Apple"
+
+    println("Ürün id : $product_id")
+    println("Ürün Adı : $product_name")
+    println("Ürün Stock Adeti : $product_stock")
+    println("Ürün Fiyatı : $product_price")
+    println("Ürün Tedarikçi : $product_supplier")
+
+    var a = 10
+    var b = 20
+
+    println("$a x $b : ${a * b}")
+
+    // Sabitler = Constant
+
+    var numberOne = 30
+    println(numberOne)
+    numberOne = 100
+    println(numberOne)
+
+    val numberTwo = 30
+    println(numberTwo)
+
+    // Tür dönüşümü = Type Casting
+
+    val i = 42
+    val d = 98.55
+    // Sayısaldan sayısala dönüşüm
+    // Dikkatli olunmalı aksi halde veri kaybına yol açabilir.
+
+    val resultOne = i.toDouble()
+    val resultTwo = d.toInt()
+    println(resultOne)
+    println(resultTwo)
+
+    // Sayısaldan metine dönüşüm
+
+    val resulThree = i.toString()
+    val resultFour = d.toString()
+    println(resulThree)
+    println(resultFour)
+
+    // Metinden sayısala dönüşüm
+
+    val input = "67.54"
+
+    val resultFive = input.toDouble()
+    println(resultFive)
+    // Kullanıcı bu sayılara bir harf girerse hata verecektir.
+    // çözümü aşşağıdaki gibidir
+
+    val resultSix = input.toDoubleOrNull()
+
+     // if koşuldur
+
+    if( resultSix != null){
+        println(resultSix)
+    }else{
+        println("Lütfen sayınızı kontrol ediniz")
+    }
+
+
+
+
+}
+
